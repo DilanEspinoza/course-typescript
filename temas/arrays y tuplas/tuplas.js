f934fb80f1d54ccd38fd32501a8f9c51927650ed
Dilan Dev <dilan.developer22@gmail.com>
2024-07-28 16:07:42 -0500

    add tuplas.js and tuplas.ts

--- temas/arrays y tuplas/tuplas.js
+++ temas/arrays y tuplas/tuplas.js
@@ -1,2 +1,2 @@
-let tupla: [number, string[]] = [1, ["nose", "algo"]];
+var tupla = [1, ["nose", "algo"]];
 // no es recomendable utilizar mas de dos o tres 3 datos en la tuplas para que no se haga mas grande
