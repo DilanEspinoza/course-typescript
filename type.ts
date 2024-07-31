1107aed01aaa64295f448e87469eaf092d5f55db
Dilan Dev <dilan.developer22@gmail.com>
2024-07-31 01:54:33 -0500

    add new themes

--- type.ts
+++ type.ts
@@ -12,3 +12,25 @@ let typescript: Cursos = {
 };
 
 console.log(typescript);
+
+// sirve para dar tipos por separado a las variables que lo lleguen a necesitar
+
+type NombreCarro = string;
+type ModeloCarro = string;
+type AnoCarro = number;
+
+type Carro = {
+	nombre: NombreCarro;
+	modelo: ModeloCarro;
+	ano: AnoCarro;
+};
+
+const nombreCarro: NombreCarro = "Adui";
+const modeloCarro: ModeloCarro = "A8";
+const anoCarro: AnoCarro = 2019;
+
+const carro: Carro = {
+	nombre: nombreCarro,
+	modelo: modeloCarro,
+	ano: anoCarro,
+};
