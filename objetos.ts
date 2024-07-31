8bd5af1f7129dd67b44289e870edb08626f2a5cc
Dilan Dev <dilan.developer22@gmail.com>
2024-07-31 17:08:50 -0500

    add file object

--- objetos.ts
+++ objetos.ts
@@ -1,4 +1,30 @@
+// Los objetos son los mismo que los de javascript
+
 type Persona = { id: number; nombre: string };
 const objeto: Persona = { id: 1, nombre: "pepelian" };
 
 console.log(objeto);
+
+// aqui es donde vamos a utilizar mayormente las interfaces
+
+/* 
+interface User{
+  id:number,
+  username:string,
+  isActive:boolean
+}
+
+const user:User ={
+  id:1,
+  username:"Pepelian",
+  isActive : false
+}
+ */
+
+// Pero tambien typescript es capas de inferir los objetos
+
+const user = {
+	id: 1,
+	username: "Pepelian",
+	isActive: false,
+};
