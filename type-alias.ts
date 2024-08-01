668d865d5a2885866fc496fe809567bed60a0959
Dilan Dev <dilan.developer22@gmail.com>
2024-08-01 09:34:01 -0500

    update file type to type-alias

---
+++ type-alias.ts
@@ -0,0 +1,36 @@
+// Type sirve para dar tipos por separado a las variables que lo lleguen a necesitar
+
+type Cursos = {
+	nombre: string;
+	tiempo: number;
+	categorias: string[];
+	calificacion?: number;
+};
+
+let typescript: Cursos = {
+	nombre: "Typescript",
+	tiempo: 1,
+	categorias: ["web", "sotfware", "javascript"],
+};
+
+console.log(typescript);
+
+type NombreCarro = string;
+type ModeloCarro = string;
+type AnoCarro = number;
+
+type Carro = {
+	nombre: NombreCarro;
+	modelo: ModeloCarro;
+	ano: AnoCarro;
+};
+
+const nombreCarro: NombreCarro = "Adui";
+const modeloCarro: ModeloCarro = "A8";
+const anoCarro: AnoCarro = 2019;
+
+const carro: Carro = {
+	nombre: nombreCarro,
+	modelo: modeloCarro,
+	ano: anoCarro,
+};
